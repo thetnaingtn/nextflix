f86572611b19a4c8da10fd55bffc3f099cc099a3
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2023-08-19 17:46:00 +0700

    layout component as client component

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,11 +1,36 @@
+'use client';
+
 import Nav from '@/app/components/nav';
+import { useRouter } from 'next/navigation';
 import { PropsWithChildren } from 'react';
+import Footer from '@/app/components/footer';
 
 export default function ProtectedLayout({ children }: PropsWithChildren) {
+  const router = useRouter();
+
+  const handleChangeCategory = (category: 'series' | 'films') => {
+    return function () {
+      router.push(`/${category}`);
+    };
+  };
+
   return (
     <>
-      <Nav />
+      <Nav>
+        <Nav.Frame>
+          <Nav.Group>
+            <Nav.Logo />
+            <Nav.TextLink onClick={handleChangeCategory('series')}>
+              Series
+            </Nav.TextLink>
+            <Nav.TextLink onClick={handleChangeCategory('films')}>
+              Films
+            </Nav.TextLink>
+          </Nav.Group>
+        </Nav.Frame>
+      </Nav>
       {children}
+      <Footer />
     </>
   );
 }
