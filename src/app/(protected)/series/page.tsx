b19cc20279c58d189e451d33715a37ae316363fe
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2023-08-20 15:00:22 +0700

    add hero to series page

--- src/app/(protected)/series/page.tsx
+++ src/app/(protected)/series/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <h1>Series Page</h1>;
+import Hero from '@/app/components/hero';
+import { getShow } from '@/lib/fetcher';
+
+export default async function Page() {
+  const allShows = await getShow('tv');
+  return (
+    <main>
+      {/* @ts-expect-error server component */}
+      <Hero type="show" shows={allShows.netflix} />
+    </main>
+  );
 }
