1416aa177a634e766243c20dea76bd45a9ac4d14
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2023-08-28 11:11:30 +0700

    add footer to layout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -30,7 +30,42 @@ export default function ProtectedLayout({ children }: PropsWithChildren) {
         </Nav.Frame>
       </Nav>
       <main className="flex-1">{children}</main>
-      <Footer />
+      <Footer>
+        <Footer.Title>Questions? Contact us.</Footer.Title>
+        <Footer.Break />
+        <Footer.Row>
+          <Footer.Column>
+            <Footer.Link href="#">FAQ</Footer.Link>
+            <Footer.Link href="#">Investor Relations</Footer.Link>
+            <Footer.Link href="#">Ways to Watch</Footer.Link>
+            <Footer.Link href="#">Corporate Information</Footer.Link>
+            <Footer.Link href="#">Netflix Originals</Footer.Link>
+          </Footer.Column>
+
+          <Footer.Column>
+            <Footer.Link href="#">Help Centre</Footer.Link>
+            <Footer.Link href="#">Jobs</Footer.Link>
+            <Footer.Link href="#">Terms of Use</Footer.Link>
+            <Footer.Link href="#">Contact Us</Footer.Link>
+          </Footer.Column>
+
+          <Footer.Column>
+            <Footer.Link href="#">Account</Footer.Link>
+            <Footer.Link href="#">Redeem gift cards</Footer.Link>
+            <Footer.Link href="#">Privacy</Footer.Link>
+            <Footer.Link href="#">Speed Test</Footer.Link>
+          </Footer.Column>
+
+          <Footer.Column>
+            <Footer.Link href="#">Media Centre</Footer.Link>
+            <Footer.Link href="#">Buy gift cards</Footer.Link>
+            <Footer.Link href="#">Cookie Preferences</Footer.Link>
+            <Footer.Link href="#">Legal Notices</Footer.Link>
+          </Footer.Column>
+        </Footer.Row>
+        <Footer.Break />
+        <Footer.Text>Netflix United Kingdom</Footer.Text>
+      </Footer>
     </div>
   );
 }
