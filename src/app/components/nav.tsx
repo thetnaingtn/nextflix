72476260ce48d196fba8e9ffbd8476a5776e7f16
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2023-08-28 15:00:34 +0700

    feat:implement search component

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,9 +1,15 @@
 import Link, { LinkProps } from 'next/link';
 import { twMerge as tm } from 'tailwind-merge';
 import Image from 'next/image';
-import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
+import {
+  HTMLAttributes,
+  InputHTMLAttributes,
+  PropsWithChildren,
+  useState,
+} from 'react';
 
 import logo from '../../../public/logo.svg';
+import searchIcon from '../../../public/search.png';
 
 function Nav({ children }: PropsWithChildren) {
   return <header>{children}</header>;
@@ -65,4 +71,81 @@ const TextLink = (
   );
 };
 
-export default Object.assign(Nav, { Logo, ButtonLink, Frame, TextLink, Group });
+// margin-left: ${({ active }) => (active === true ? '10px' : '0')};
+//   padding: ${({ active }) => (active === true ? '0 10px' : '0')};
+//   opacity: ${({ active }) => (active === true ? '1' : '0')};
+//   width: ${({ active }) => (active === true ? '200px' : '0px')};
+
+function Search({
+  children,
+  className,
+  ...restProps
+}: PropsWithChildren<HTMLAttributes<HTMLDivElement>>) {
+  const [searchActive, setSearchActive] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  return (
+    <div className={tm('flex items-center', className)} {...restProps}>
+      <SearchIcon
+        className="text-white"
+        onClick={() => setSearchActive(!searchActive)}
+      >
+        <Image
+          src={searchIcon}
+          alt="search-icon"
+          className="w-4 invert brightness-0"
+        />
+      </SearchIcon>
+      <SearchInput
+        value={searchTerm}
+        className={
+          searchActive ? 'ml-[10px] p-[10px] opacity-100 w-[200px]' : ''
+        }
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Search films and series"
+      />
+    </div>
+  );
+}
+
+function SearchInput({
+  className,
+  ...restProps
+}: InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      type="text"
+      className={tm(
+        'text-white bg-[rgba(64,64,64,0.5)] border-solid border border-white h-[30px] text-sm rounded ml-0 p-0 opacity-0 w-0 transition-[width] duration-500 focus:bg-[rgba(0,0,0,0.8)]',
+        className
+      )}
+      {...restProps}
+    />
+  );
+}
+
+function SearchIcon({
+  className,
+  children,
+  ...restProps
+}: PropsWithChildren<HTMLAttributes<HTMLButtonElement>>) {
+  return (
+    <button
+      className={tm(
+        'cursor-pointer bg-transparent border-0 outline-0 h-8 w-8 p-0 flex items-center justify-center',
+        className
+      )}
+      {...restProps}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Object.assign(Nav, {
+  Logo,
+  ButtonLink,
+  Frame,
+  TextLink,
+  Group,
+  Search,
+});
