1bb3f8b89ae53b0391b0b3e40ca3eedb4582d654
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2023-08-29 15:05:01 +0700

    feat: modify hero component layout

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,7 +1,10 @@
-import { Show } from '@/lib/fetcher';
 import Image from 'next/image';
-import { PropsWithChildren } from 'react';
 import { twMerge as tm } from 'tailwind-merge';
+
+import PlayIcon from '@/app/components/icons/play';
+import ExclamationCircleIcon from '@/app/components/icons/exclamation-circle';
+
+import type { Show } from '@/types';
 
 type HeroProps =
   | {
@@ -29,8 +32,8 @@ export default async function Hero(props: HeroProps) {
             )}
           />
           <Image
-            src={`https://image.tmdb.org/t/p/original/${
-              randomShow?.poster_path ?? randomShow?.backdrop_path ?? ''
+            src={`https://image.tmdb.org/t/p/original${
+              randomShow?.backdrop_path ?? randomShow?.poster_path ?? ''
             }`}
             alt={randomShow?.title ?? 'poster'}
             className="h-auto w-full object-cover"
@@ -39,26 +42,31 @@ export default async function Hero(props: HeroProps) {
           />
         </div>
         <div className="grid max-w-lg space-y-2 pt-24">
-          <h1 className="text-3xl font-bold md:text-4xl">
+          <h1 className="text-3xl font-bold md:text-4xl text-white">
             {randomShow?.title ?? randomShow?.name}
           </h1>
           <div className="flex space-x-2 text-xs font-semibold md:text-sm">
             <p className="text-green-600">
               {Number(randomShow?.vote_average * 10).toFixed(2) ?? '-'}% Match
             </p>
-            <p className="text-gray-300">{randomShow?.release_date ?? '-'}</p>
+            <p className="text-gray-300">{randomShow?.first_air_date ?? '-'}</p>
           </div>
           <p className="line-clamp-4 text-sm text-gray-300 md:text-base">
             {randomShow?.overview ?? '-'}
           </p>
           <div className="flex items-center space-x-2 pt-1.5">
-            <button aria-label="Play video" className="h-auto gap-1.5 rounded">
+            <button
+              aria-label="Play video"
+              className="h-auto gap-1.5 rounded bg-white px-4 py-2 flex items-center justify-center text-sm font-medium"
+            >
+              <PlayIcon />
               Play
             </button>
             <button
               aria-label="Open show's details modal"
-              className="h-auto gap-2 rounded"
+              className="h-auto gap-2 rounded border border-solid border-white px-4 py-2 text-white flex items-center justify-center"
             >
+              <ExclamationCircleIcon />
               More Info
             </button>
           </div>
