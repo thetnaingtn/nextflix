b5a60dd881fd51f7191fddeab45b7022bd4c633f
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2023-08-30 14:54:30 +0700

    remove unnecessary style definition

--- src/app/components/accordion/accordion.tsx
+++ src/app/components/accordion/accordion.tsx
@@ -75,33 +75,6 @@ export function Item({ children, className, ...restProps }: DivProps) {
   );
 }
 
-`
-  display: flex;
-  justify-content: space-between;
-  cursor: pointer;
-  margin-bottom: 1px;
-  font-size: 26px;
-  font-weight: normal;
-  background: #303030;
-  padding: 0.8em 1.2em 0.8em 1.2em;
-  user-select: none;
-  align-items: center;
-
-  img {
-    filter: brightness(0) invert(1);
-    width: 24px;
-    user-select: none;
-
-    @media (max-width: 600px) {
-      width: 16px;
-    }
-  }
-
-  @media (max-width: 600px) {
-    font-size: 16px;
-  }
-`;
-
 export function Header({ children, className, ...restProps }: DivProps) {
   const { toggle, setToggle } = useContext(ToggleContext)!;
 
@@ -135,34 +108,6 @@ export function Header({ children, className, ...restProps }: DivProps) {
     </div>
   );
 }
-
-`font-size: 26px;
-font-weight: normal;
-line-height: normal;
-background: #303030;
-white-space: pre-wrap;
-user-select: none;
-overflow: hidden;
-
-&.closed {
-  max-height: 0;
-  overflow: hidden;
-  transition: max-height 0.25s cubic-bezier(0.5, 0, 0.1, 1);
-}
-&.open {
-  max-height: 1200px;
-  transition: max-height 0.25s cubic-bezier(0.5, 0, 0.1, 1);
-}
-
-span {
-  display: block;
-  padding: 0.8em 2.2em 0.8em 1.2em;
-}
-
-@media (max-width: 600px) {
-  font-size: 16px;
-  line-height: 22px;
-}`;
 
 export function Body({ children, className, ...restProps }: DivProps) {
   const { toggle } = useContext(ToggleContext)!;
