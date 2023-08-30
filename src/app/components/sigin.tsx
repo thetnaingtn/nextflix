275aef637c924a2a551f2291129b356c2a23325b
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2023-08-30 16:53:21 +0700

    signin component

---
+++ src/app/components/sigin.tsx
@@ -0,0 +1,40 @@
+'use client';
+import Form from '@/app/components/form';
+import { useState } from 'react';
+
+export default function Sigin() {
+  const [error, setError] = useState(null);
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSignin = () => null;
+  return (
+    <Form>
+      <Form.Title>Sign In</Form.Title>
+      {error && <Form.Error data-testid="error">{error}</Form.Error>}
+
+      <Form.Base onSubmit={handleSignin} method="POST">
+        <Form.Input
+          placeholder="Email address"
+          value={emailAddress}
+          onChange={({ target }) => setEmailAddress(target.value)}
+        />
+        <Form.Input
+          type="password"
+          value={password}
+          autoComplete="off"
+          placeholder="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <Form.Submit type="submit" data-testid="sign-in">
+          Sign In
+        </Form.Submit>
+      </Form.Base>
+      <Form.Text>
+        New to Netflix? <Form.Link href="/signup">Sign up now.</Form.Link>
+      </Form.Text>
+      <Form.TextSmall>
+        {`This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.`}
+      </Form.TextSmall>
+    </Form>
+  );
+}
