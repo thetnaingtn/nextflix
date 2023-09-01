c657bcd8c8c717db93deb43c6276d5bb9f071e62
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2023-09-01 16:41:39 +0700

    implement and add modal to collection

--- src/app/(protected)/_components/collections.tsx
+++ src/app/(protected)/_components/collections.tsx
@@ -1,11 +1,14 @@
+'use client';
+
 import Image from 'next/image';
 import _truncate from 'lodash.truncate';
 import _shuffle from 'lodash.shuffle';
+import { useState } from 'react';
 
 import Card from '@/app/components/card';
 import { Show } from '../../../types';
-import { getSearchedResult } from '@/lib/fetcher';
 import { getSafeImageUrl } from '@/lib/util';
+import ShowModal from './show-modal';
 
 interface Collection {
   title: string;
@@ -19,10 +22,13 @@ interface CollectionsProps {
 
 const TRUNCATE_OPTIONS = { length: 149 };
 
-export default async function Collections({
+export default function Collections({
   collections,
   searchedResults,
 }: CollectionsProps) {
+  const [toggleModal, setToggleModal] = useState(false);
+  const [selectedShow, setSelectedShow] = useState<Show>({} as Show);
+
   if (searchedResults.length > 0) {
     return (
       <Card.Group>
@@ -54,7 +60,6 @@ export default async function Collections({
             })}
           </Card.Entities>
         </Card>
-        ;
       </Card.Group>
     );
   }
@@ -66,32 +71,42 @@ export default async function Collections({
           <Card.Title>{collection.title}</Card.Title>
           <Card.Entities>
             {/* temporarily cut off shows */}
-            {_shuffle(collection.shows)
-              .slice(0, 5)
-              .map((show) => {
-                const src = getSafeImageUrl(show.backdrop_path);
-                return (
-                  <Card.Item data-testid="item" key={show.id}>
-                    <Card.Image>
-                      <Image
-                        width={305}
-                        height={200}
-                        src={src}
-                        alt={show.title ?? 'show-image'}
-                      />
-                    </Card.Image>
-                    <Card.Meta>
-                      <Card.SubTitle>{show.title ?? show.name}</Card.SubTitle>
-                      <Card.Text>
-                        {_truncate(show.overview ?? '', TRUNCATE_OPTIONS)}
-                      </Card.Text>
-                    </Card.Meta>
-                  </Card.Item>
-                );
-              })}
+            {collection.shows.slice(0, 5).map((show) => {
+              const src = getSafeImageUrl(show.backdrop_path);
+              return (
+                <Card.Item
+                  onClick={() => {
+                    setToggleModal(true);
+                    setSelectedShow(show);
+                  }}
+                  data-testid="item"
+                  key={show.id}
+                >
+                  <Card.Image>
+                    <Image
+                      width={305}
+                      height={200}
+                      src={src}
+                      alt={show.title ?? 'show-image'}
+                    />
+                  </Card.Image>
+                  <Card.Meta>
+                    <Card.SubTitle>{show.title ?? show.name}</Card.SubTitle>
+                    <Card.Text>
+                      {_truncate(show.overview ?? '', TRUNCATE_OPTIONS)}
+                    </Card.Text>
+                  </Card.Meta>
+                </Card.Item>
+              );
+            })}
           </Card.Entities>
         </Card>
       ))}
+      <ShowModal
+        show={selectedShow}
+        toggle={toggleModal}
+        toggleHandler={setToggleModal}
+      />
     </Card.Group>
   );
 }
---
+++ src/app/(protected)/_components/show-modal.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import type { Dispatch, SetStateAction } from 'react';
+import ReactPlayer from 'react-player/lazy';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+} from '@/app/components/dialog';
+import type { Show } from '@/types';
+
+interface ShowModalProps {
+  show: Show;
+  toggleHandler: Dispatch<SetStateAction<boolean>>;
+  toggle: boolean;
+}
+
+export default function ShowModal({
+  show,
+  toggle,
+  toggleHandler,
+}: ShowModalProps) {
+  const [trailer, setTrailer] = useState('');
+  const [genres, setGenres] = useState<{ id: number; name: string }[]>([]);
+
+  useEffect(() => {
+    if (!toggle) return;
+    (async function () {
+      const resp = await fetch(
+        `https://api.themoviedb.org/3/${
+          show?.media_type === 'tv' ? 'tv' : 'movie'
+        }/${show?.id}?api_key=${
+          process.env.NEXT_PUBLIC_TMDB_API_KEY
+        }&language=en-US&append_to_response=videos`
+      );
+      const data = await resp.json();
+      console.log(data);
+      if (data?.videos) {
+        setTrailer(
+          data?.videos.results.find((video: any) => video.type === 'Trailer')
+            ?.key
+        );
+      }
+      if (data?.genres) {
+        setGenres(data?.genres);
+      }
+    })();
+  }, [show.id, show.media_type, toggle]);
+
+  return (
+    <Dialog open={toggle} onOpenChange={toggleHandler}>
+      <DialogContent className="w-full overflow-hidden rounded-md bg-zinc-900 p-0 text-left align-middle shadow-xl dark:bg-zinc-900 sm:max-w-3xl">
+        <div className="aspect-video relative">
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${trailer}`}
+            width="100%"
+            height="100%"
+          />
+        </div>
+        <div className="grid gap-2.5 px-10 pb-10">
+          <DialogTitle className="text-lg font-medium leading-6 text-slate-50 sm:text-xl">
+            {show?.title ?? show?.name}
+          </DialogTitle>
+          <div className="flex items-center space-x-2 text-sm sm:text-base">
+            <p className="font-semibold text-green-400">
+              {Math.round((Number(show?.vote_average) / 10) * 100) ?? '-'}%
+              Match
+            </p>
+            {show?.release_date ? (
+              <p>{show?.release_date}</p>
+            ) : show?.first_air_date ? (
+              <p>{show?.first_air_date}</p>
+            ) : null}
+            {show?.original_language && (
+              <span className="grid h-4 w-7 place-items-center text-xs font-bold text-neutral-400 ring-1 ring-neutral-400">
+                {show.original_language.toUpperCase()}
+              </span>
+            )}
+          </div>
+          <DialogDescription className="line-clamp-3 text-xs text-slate-50 dark:text-slate-50 sm:text-sm">
+            {show?.overview ?? '-'}
+          </DialogDescription>
+          <div className="flex items-center gap-2 text-xs sm:text-sm text-white">
+            <span className="text-slate-400">Genres:</span>
+            {genres.map((genre) => genre.name).join(', ')}
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
