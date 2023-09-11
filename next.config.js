0ca34b47c408b6d63839353d1bc27cec2ce81bb1
thetnaingtn <thetnaingtun.ucsy@gmail.com>
2023-09-11 15:07:07 +0700

    use unoptimize image for static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true, // disabled for static export. we don't have custom loader at the moment.
     domains: ['image.tmdb.org', 'via.placeholder.com'],
   },
 };
